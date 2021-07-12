cdc55d40bc88678b85e3ec57b35da295074c5184
haseom <gudejr31@naver.com>
2021-07-12 17:11:25 +0900

    7/12 class_2 class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -15,10 +17,7 @@ def hello_world(request):
         new_data.text = temp
         new_data.save()
 
-        data_list = HelloWorld.objects.all()
-
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
