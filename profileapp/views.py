db37103ee2b62375f41eec9171139203f7c578f2
haseom <gudejr31@naver.com>
2021-07-28 12:45:51 +0900

    7/28 class_2 class commit 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = profile
     form_class = ProfileCreationForm
@@ -21,6 +26,8 @@ class ProfileCreateView(CreateView):
         return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = profile
     context_object_name = 'target_profile'
