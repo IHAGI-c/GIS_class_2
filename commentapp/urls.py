6e2fc7f6a461a28770fa5d556642ef8de3b3724a
haseom <gudejr31@naver.com>
2021-08-09 17:13:39 +0900

    8/9 class_2 class commit 2

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView
+
+app_name = 'createapp'
 
 urlpatterns = [
-
+    path('create/', CommentCreateView.as_view(), name='create'),
 ]
